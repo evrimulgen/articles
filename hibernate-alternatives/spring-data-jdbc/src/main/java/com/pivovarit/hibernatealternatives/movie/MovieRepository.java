159f71657c4ff7cd49ec21e81d86243e326a369b
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-08-15 11:41:47 +0200

    spring data jdbc fix

--- hibernate-alternatives/spring-data-jdbc/src/main/java/com/pivovarit/hibernatealternatives/movie/MovieRepository.java
+++ hibernate-alternatives/spring-data-jdbc/src/main/java/com/pivovarit/hibernatealternatives/movie/MovieRepository.java
@@ -1,9 +1,15 @@
 package com.pivovarit.hibernatealternatives.movie;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
+    @Query("SELECT * FROM movies WHERE title=:title")
+    List<Movie> findByTitle(@Param("title") String title);
 }
